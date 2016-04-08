a4426b1bd60147b70156bfb2b00213054adf7ecf
jjtny1 <JobyThundil@Jobys-MacBook-Air.local>
2016-04-08 15:25:56 -0400

    fix type

--- example.py
+++ example.py
@@ -3,6 +3,6 @@ from Bitfinex import Bitfinex
 BFX = Bitfinex('API-KEY', 'API-SECRET', 'https://api.bitfinex.com/v1')
 
 print BFX.order_book()
-print BFX.open_orders()
+print BFX.open_order()
 resp = BFX.buy('BTCUSD', 1.03, 400, "exchange market")
 print resp
